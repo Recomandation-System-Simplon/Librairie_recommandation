ef7bcccb7175e5a995ffcc2ae09e04663f7693c7
Julien B <julien@bosse.pro>
2021-09-28 16:37:47 +0200

    add collaborative filtering

--- DossierWorks/Recommandation.py
+++ DossierWorks/Recommandation.py
@@ -13,19 +13,31 @@ Brief Projet Simplon : Application recommandation
 
 import pandas as pd
 import numpy as np
+import pyarrow.feather as feather
 
 cos_sim = np.load("data/cos_sim.npy")
 index_books = pd.read_csv("data/index_books.csv")
 books = pd.read_csv("data/books.csv")
 popularity_books = pd.read_csv("data/popularity_books.csv")
 
+ratings = pd.read_csv("data/ratings.csv")
+original_ratings = ratings.pivot(index="user_id",columns="book_id",values="rating").fillna(0)
+predicted_ratings = feather.read_feather("data/predicted_ratings")
+predicted_ratings = pd.DataFrame(predicted_ratings, columns=original_ratings.columns, index=original_ratings.index)
+
 def get_book_id(title): 
     # Récupère l'ID du livre en fonction de son titre
     return books["goodreads_book_id"][books["title"]==title].item()
 
-def get_book_title(goodreads_id):
-    # Récupère le titre du livre en fonction de son id
+def get_book_title_GR(goodreads_id):
+    # Récupère le titre du livre en fonction de son ID
     return books["title"][books["goodreads_book_id"]==goodreads_id].item()
+
+def get_book_title(book_id):
+    # Fonction qui récupère le titre du livre grâce à son id
+    book_name = books["title"][books["book_id"]==book_id].item()
+    return book_name
+
 
 def get_book_weighted_rating(goodreads_id):
     try:
@@ -62,6 +74,12 @@ class Recommandation:
 
             elif self.is_user_new == "N":
                 self.user = int(input("\nUser ID please : "))
+                list_prefered, list_recommandations = self.collaborative(user=self.user)
+                print("\nYour favorite books are : ")
+                print(list_prefered)
+                print("\nSo we recommand : ")
+                print(list_recommandations)
+                
 
             elif self.is_user_new == "0":
                 self.user_bool = False
@@ -91,9 +109,9 @@ class Recommandation:
 
         rec_books_ids = list(map(lambda x: index_books["book"][x],rec_books_indexes))
 
-        rec_books = list(map(get_book_title, rec_books_ids))
+        rec_books = list(map(get_book_title_GR, rec_books_ids))
 
-        return list(rec_books)
+        return rec_books
 
 
     def hybrid(self, title, cos_sim = cos_sim):
@@ -116,7 +134,7 @@ class Recommandation:
         # Création de la série qui comprend la similiraté de notre livre et tous les autres (correspond à sa colonne de la matrice cos_sim)
         score_series = pd.Series(cos_sim[idx]).sort_values(ascending = False)
 
-        # On récupère les 10 premiers indices (plus grandes similarités)
+        # On récupère les 30 premiers indices (plus grandes similarités)
         rec_books_indexes = list(score_series.iloc[1:31].index)
         
         rec_books_ids = list(map(lambda x: index_books["book"][x],rec_books_indexes))
@@ -124,12 +142,32 @@ class Recommandation:
         # Liste des weighted ratings associés aux livres
         rec_books_wr = list(map(get_book_weighted_rating,rec_books_ids))
 
-
         rec_books = pd.DataFrame(list(zip(rec_books_ids, rec_books_wr)),columns=['id','wr'])
         rec_books = rec_books.sort_values("wr",ascending=False)
-        rec_books = list(map(get_book_title,list(rec_books["id"][0:5])))
+        rec_books = list(map(get_book_title_GR,list(rec_books["id"][0:5])))
 
         return rec_books
+        
+    def collaborative(self, user, predicted_ratings = predicted_ratings, original_ratings = original_ratings):
+
+        list_recommandations = []
+
+        # Récupère la ligne qui correspond à l'user dans les notes originales et les notes prédites
+        or_user = original_ratings.loc[user]
+        pr_user = predicted_ratings.loc[user]
+
+        prefered_books = pr_user[or_user!=0].sort_values(axis=0, ascending=False)
+        pr_user = pr_user[or_user==0].sort_values(axis=0, ascending=False) # On garde les notes qui n'étaient pas présentes dans les notes originales et on les trie
+
+        # On récupère les indices (= id) des 10 premiers 
+        list_prefered = list(prefered_books[0:10].index)
+        list_recommandations = list(pr_user[0:10].index)
+
+        # On récupère leurs noms
+        list_prefered = list(map(get_book_title,list_prefered))
+        list_recommandations = list(map(get_book_title,list_recommandations))
+
+        return list_prefered, list_recommandations
     
 
 Recommandation()
